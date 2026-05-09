eac7adb7814e3d7904e87ed05cca7276b79e6578
SrivaSar <sarthaksrivastava344@gmail.com>
2026-05-09 20:46:28 +0530

    kkkk

---
+++ ai-hybrid-test-automation-framework/src/test/java/pages/hrmLoginpage.java
@@ -0,0 +1,9 @@
+package pages;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+public class hrmLoginpage {
+    WebDriver driver;
+    public hrmLoginpage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
